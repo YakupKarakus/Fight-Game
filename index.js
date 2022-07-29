6c887d4a5abc17b0dc4a8c2165c379ba01affdbb
yakup karakuş <ykp_krk@hotmail.com>
2022-07-29 23:58:57 +0300

    movment added to player

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ canvas.height=576;
 
 c.fillRect(0,0,canvas.width,canvas.height);
 
-
+const gravity=0.2
 class Sprite{
     constructor({position,velocity}){
         this.position=position
         this.velocity=velocity
+        this.height=150
     }
 
     draw(){
         c.fillStyle='red'
-        c.fillRect(this.position.x,this.position.y,50,150)
+        c.fillRect(this.position.x,this.position.y,50,this.height)
+    }
+    update(){
+        this.draw()
+        
+        this.position.x+=this.velocity.x
+        this.position.y+=this.velocity.y
+
+        if(this.position.y+this.height+this.velocity.y >= canvas.height){
+            this.velocity.y=0
+        }else this.velocity.y+=gravity
     }
 }
 
@@ -26,10 +37,10 @@ const player=new Sprite({
 },
 velocity:{
     x:0,
-    y:0
+    y:10
 }});
 
-player.draw();
+
 
 const enemy=new Sprite({
     position:{
@@ -44,8 +55,56 @@ const enemy=new Sprite({
 enemy.draw();
 console.log(player);
 
+const keys={
+    a:{
+        pressed:false
+    },
+    d:{
+        pressed:false
+    }
+}
+
+
+
 function animate(){
     window.requestAnimationFrame(animate)
-    console.log('go')
+    c.fillStyle='black'
+    c.fillRect(0,0,canvas.width,canvas.height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x=0
+    if(keys.a.pressed){
+        player.velocity.x=-1
+    }else if(keys.d.pressed){
+        player.velocity.x=1
+    }
 }
 animate()
+
+window.addEventListener('keydown',(event)=>{
+    switch(event.key){
+        case 'd':
+            keys.d.pressed=true
+            break
+            case 'a':
+            keys.a.pressed=true
+            break
+    }
+    
+    console.log(event.key);
+})
+window.addEventListener('keyup',(event)=>{
+    switch(event.key){
+        case 'd':
+            keys.d.pressed=false
+            break
+        case 'a':
+            keys.a.pressed=false
+            break    
+            
+            
+    }
+    
+    console.log(event.key);
+})

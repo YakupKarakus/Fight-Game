553ecb8d326085b5efb0ce381f9b8bfd5364bc64
yakup karakuş <ykp_krk@hotmail.com>
2022-07-30 00:02:51 +0300

    added lastkey variable to let move character

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const keys={
     }
 }
 
-
+let lastKey
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -74,9 +74,9 @@ function animate(){
     enemy.update()
 
     player.velocity.x=0
-    if(keys.a.pressed){
+    if(keys.a.pressed && lastKey==='a'){
         player.velocity.x=-1
-    }else if(keys.d.pressed){
+    }else if(keys.d.pressed && lastKey==='d'){
         player.velocity.x=1
     }
 }
@@ -86,9 +86,11 @@ window.addEventListener('keydown',(event)=>{
     switch(event.key){
         case 'd':
             keys.d.pressed=true
+            lastKey='d'
             break
             case 'a':
             keys.a.pressed=true
+            lastKey='a'
             break
     }
     

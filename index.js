6f41f2614957ee9cded126cf573e0f56478e833f
yakup karakuş <ykp_krk@hotmail.com>
2022-07-30 21:58:38 +0300

    assers and background folders added

--- index.js
+++ index.js
@@ -1,69 +1,23 @@
 const canvas=document.querySelector('canvas');
 const c=canvas.getContext('2d');
 
-canvas.width=1890;
-canvas.height=576;
+canvas.width=1024;
+canvas.height=683;
 
 c.fillRect(0,0,canvas.width,canvas.height);
 
 const gravity=0.7
-class Sprite{
-    constructor({position,velocity,color='red',offset}){
-        this.position=position
-        this.velocity=velocity
-        this.width=50
-        this.height=150
-        this.lastKey
-        this.attackBox={
-            position:{
-                x:this.position.x,
-                y:this.position.y
-            } ,
-            offset,
-            width:100,
-            height:50
-        }
-        this.color=color
-        this.isAttacking
-        this.health=100
-    }
 
-    draw(){
-        c.fillStyle=this.color
-        c.fillRect(this.position.x,this.position.y,50,this.height)
+const background=new Sprite({
+    position:{
+        x:0,
+        y:0
+    },
+    imageSrc:'./img/background/background_layer_4.jpg'
+    
+})
 
-        //attack box
-        if(this.isAttacking) {
-        
-            c.fillStyle='green'
-            c.fillRect(this.attackBox.position.x,
-            this.attackBox.position.y,
-            this.attackBox.width,
-            this.attackBox.height)
-         }
-        
-    }
-    update(){
-        this.draw()
-        this.attackBox.position.x=this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y=this.position.y
-        
-        this.position.x+=this.velocity.x
-        this.position.y+=this.velocity.y
-
-        if(this.position.y+this.height+this.velocity.y >= canvas.height){
-            this.velocity.y=0
-        }else this.velocity.y+=gravity
-    }
-    attack(){
-        this.isAttacking=true
-        setTimeout(() => {
-           this.isAttacking=false 
-        }, 100);
-    }
-}
-
-const player=new Sprite({
+const player=new Fighter({
     position:{
     x:0,
     y:0 
@@ -79,7 +33,7 @@ offset:{
 
 
 
-const enemy=new Sprite({
+const enemy=new Fighter({
     position:{
         x:400,
         y:100 
@@ -119,7 +73,8 @@ function rectangularCollision({rectangle1,rectangle2}){
         rectangle1.attackBox.position.y+rectangle1.attackBox.height >= rectangle2.position.y && 
         rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
 }
-function determineWinner({player,enemy}){
+function determineWinner({player,enemy,timerId }){
+    clearTimeout(timerId)
     document.querySelector('#displayText').style.display='flex'
     if(player.health===enemy.health){
         document.querySelector('#displayText').innerHTML='Tie'
@@ -137,6 +92,7 @@ function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle='black'
     c.fillRect(0,0,canvas.width,canvas.height)
+    background.update()
     player.update()
     enemy.update()
 
@@ -170,22 +126,23 @@ function animate(){
     }
     // end game based on health
     if(enemy.health<=0 || player.health<=0){
-        determineWinner({player,enemy})
+        determineWinner({player,enemy,timerId})
     }
 }
 let timer=60
+let timerId
 function decreaseTimer(){
     
 
-    
+
     if(timer>0 ) {
-        setTimeout(decreaseTimer,1000)
+        timerId=setTimeout(decreaseTimer,1000)
         timer--
         document.querySelector('#timer').innerHTML=timer
     }
     if(timer===0)
     {
-        determineWinner({player,enemy})
+        determineWinner({player,enemy,timerId})
     }
     
 }
